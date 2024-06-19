f61fdd1c8d7fe4cae8f8eebad07b9e1a8d9c8b55
Alefhi Lima <alefhilima@gmail.com>
2024-06-19 14:45:38 -0400

    Subindo o projeto para o github

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nubank
+
+<div>
+  <img src="./img/website.png"/>
+</div>
+
+Projeto desenvolvido no curso FrontPush, junto ao instrutor Nasser, utilizando tecnologias como HTML, CSS, SASS e JAVASCRIPT.
---
+++ js/slide.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper-container", {
+    slidersPerView: "auto",
+    spaceBetween: 40,
+    grabCursor: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickable: true,
+        type: "progressbar"
+    },
+    navigation: {
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+    }
+})
